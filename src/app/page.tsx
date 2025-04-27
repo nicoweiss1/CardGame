c5c0dc4bcf5f2e7f235a8a093fb7959c1b7ea329
nicoweiss1 <148877618+nicoweiss1@users.noreply.github.com>
2025-04-27 17:22:35 +0200

    Ablage Stapel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,9 @@ export default function CardGame() {
   const [deckId, setDeckId] = useState<string | null>(null); // speichert id des aktuellen Kartendecks
   const [player1Cards, setPlayer1Cards] = useState<Card[]>([]); // speichert gezogene Karten von Player1
   const [player2Cards, setPlayer2Cards] = useState<Card[]>([]); // speichert gezogene Karten von Player 2
-  const [loading, setLoading] = useState<boolean>(false);
-  const [gameStarted, setGameStarted]= useState<boolean>(false);
+  const [ablageKarte, setAblageKarte] = useState<Card | null>(null) // speichert die Ablage Karte, Card ohne[] weil es nur eine AblageKarte gibt
+  const [loading, setLoading] = useState<boolean>(false); // speichert ob gerade spiel Button gedrückt wurde
+  const [gameStarted, setGameStarted]= useState<boolean>(false); // speichert ob gerade Spiel gestartet wurde
   
 
   // Neues Deck erstellen
@@ -32,19 +33,20 @@ export default function CardGame() {
     setLoading(true); // loading zu true
     setGameStarted(true);
 
-    const res = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=10`); // Bringen das deck zu "res" mit der deckId
+    const res = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=11`); // Bringen das deck zu "res" mit der deckId
     const data = await res.json() // in "data" wird die Reponse also antwort von der API in ein json umgewandelt
 
-    if (data.cards.length === 10) { // Wenn Anzahl Karten 10 ist dann:
+    if (data.cards.length === 11) { // Wenn Anzahl Karten 10 ist dann:
       setPlayer1Cards(data.cards.slice(0, 5)); // "player1Cards" bekommt erste 5 Karten index 0-4
-      setPlayer2Cards(data.cards.slice(5, 10)); // "player2Cards" bekommt letzte 5 Karten index 5-
+      setPlayer2Cards(data.cards.slice(5, 10)); // "player2Cards" bekommt letzte 5 Karten index 5-9
+      setAblageKarte(data.cards[10]); // 11. Karte wird zu "ablageKarte" hinzugefügt
     }
 
     setLoading(false); // "loading" wird zu false
   }
 
   return (
-    <div className="flex min-h-screen bg-black text-white p-6">
+    <div className="flex min-h-screen bg-black text-white p-6"> {/*p-6 innenabstand 6 also von aussen nach innen 6 abstand*, min-h-screen mindest höhe immer 100% des bildschirm, also egal wie wenig inhalt container immer 100% höhe des Bildschirms/}
 
       {/* Spieler 1 links */}
       {gameStarted && (
@@ -57,8 +59,7 @@ export default function CardGame() {
       )}
 
       {/* Spielfeld Mitte */}
-      <div className="flex-1 flex flex-col items-center justify-center">
-
+      <div className="flex-1 flex flex-col items-center justify-center"> {/*flex-1 container dehnt sich so weit wie möglich aus, weil wir haben bei Spieler 1 und 2 eine Fest Breite w-32*/}
         {/* Vor Spielstart: Button */}
         {!gameStarted && (
           <>
@@ -72,12 +73,16 @@ export default function CardGame() {
             </button>
           </>
         )}
-
         {/* Nach Spielstart: Weißer Tisch */}
         {gameStarted && (
-          <div className="w-[300px] h-[400px] bg-white rounded-xl shadow-lg flex items-center justify-center">
-            {/* Platzhalter für später: Ablagestapel, Buttons, etc. */}
-            <p className="text-black font-semibold">Tischbereich</p>
+          <div className="flex items-center justify-between w-[300px] h-[250px] bg-white rounded-xl shadow-lg p-4">
+            {ablageKarte && (
+              <img src={ablageKarte.image} alt={ablageKarte.code} className="w-24 h-auto"/>
+            )}
+            {/* Dummy für Nachziehstapel */}
+            <div className="w-24 h-34 bg-gray-300 rounded-lg flex items-center justify-center">
+              <p className="text-black font-bold">Deck</p>
+            </div>
           </div>
         )}
       </div>
